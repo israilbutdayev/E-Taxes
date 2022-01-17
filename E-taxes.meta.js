dc8c0697258e8adad13ff75ea283984b2f480062
israilbutdayev <84040990+israilbutdayev@users.noreply.github.com>
2022-01-17 21:15:16 +0400

    Update E-taxes.meta.js

--- E-taxes.meta.js
+++ E-taxes.meta.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         E-taxes
 // @namespace    http://tampermonkey.net/
-// @version      0.0.2
+// @version      0.0.1
 // @description  050 636 09 56
 // @run-at       document-start
 // @author       Israil Butdayev
 // @match        https://qaime.e-taxes.gov.az/*
 // @match        https://www.e-taxes.gov.az/vedop2/ebyn/dispatch*
+// @updateURL    https://github.com/israilbutdayev/E-Taxes/raw/main/E-taxes.meta.js
+// @downloadURL  https://github.com/israilbutdayev/E-Taxes/raw/main/E-taxes.main.js
 // @icon         https://qaime.e-taxes.gov.az/assets/images/placeholder.jpg
 // @grant        none
 // ==/UserScript==
