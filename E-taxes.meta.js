67bab400b5d74882d88cbe4897cfe9836433ac01
israilbutdayev <84040990+israilbutdayev@users.noreply.github.com>
2022-01-17 21:18:44 +0400

    Update E-taxes.meta.js

--- E-taxes.meta.js
+++ E-taxes.meta.js
@@ -7,8 +7,8 @@
 // @author       Israil Butdayev
 // @match        https://qaime.e-taxes.gov.az/*
 // @match        https://www.e-taxes.gov.az/vedop2/ebyn/dispatch*
-// @updateURL    https://github.com/israilbutdayev/E-Taxes/raw/main/E-taxes.meta.js
-// @downloadURL  https://github.com/israilbutdayev/E-Taxes/raw/main/E-taxes.main.js
+// @updateURL    https://raw.githubusercontent.com/israilbutdayev/E-Taxes/main/E-taxes.meta.js
+// @downloadURL  https://raw.githubusercontent.com/israilbutdayev/E-Taxes/main/E-taxes.main.js
 // @icon         https://qaime.e-taxes.gov.az/assets/images/placeholder.jpg
 // @grant        none
 // ==/UserScript==
